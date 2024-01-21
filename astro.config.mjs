c68f15c3578417346632af546a96c65e524714e4
maxdemaio <maxdman321@gmail.com>
2024-01-20 22:37:36 -0500

    Add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,19 @@ import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 import sitemap from '@astrojs/sitemap';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   site: 'https://music.maxdemaio.com',
-  integrations: [
-    UnoCSS({
-      injectReset: true,
-    }),
-    sitemap(),
-  ],
+  integrations: [UnoCSS({
+    injectReset: true
+  }), sitemap()],
   markdown: {
     shikiConfig: {
-      theme: 'github-dark-dimmed',
-    },
+      theme: 'github-dark-dimmed'
+    }
   },
+  adapter: vercel()
 });
