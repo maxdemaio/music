fe1d84d85b23e94ef204913a2f9601a23bda825f
maxdemaio <maxdman321@gmail.com>
2025-05-18 08:53:02 -0400

    trailing slash preference and sitemap link

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  trailingSlash: "never",
   site: 'https://music.maxdemaio.com',
   integrations: [UnoCSS({
     injectReset: true
